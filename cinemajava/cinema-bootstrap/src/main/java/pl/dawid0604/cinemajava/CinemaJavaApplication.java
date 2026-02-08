dc8d1b02cb3d254edad2531ec2677a73e70b1fc3
Dawid0604 <tadek123tadek@o2.pl>
2026-02-08 16:07:32 +0100

    feat: main class implementation

---
+++ cinemajava/cinema-bootstrap/src/main/java/pl/dawid0604/cinemajava/CinemaJavaApplication.java
@@ -0,0 +1,13 @@
+/* Copyright 2026 CinemaJava */
+package pl.dawid0604.cinemajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaJavaApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CinemaJavaApplication.class, args);
+    }
+}
